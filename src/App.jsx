b4891470d16cf09c75ec64bb3a37e033566eb591
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-01-02 21:27:31 +0000

    make a start to new portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <main className='app'>
+      <section className="main__content">
+        <p>New Portfolio</p>
+      </section>
+      
+    </main>
+  )
+}
+
+export default App
