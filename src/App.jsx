470fee51d29d3906efcd37fc113025e088274917
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-01-09 21:29:07 +0000

    complete hero section for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,12 @@ const App = () => {
   return (
     <main className="app overflow-hidden">
       <Background showNav={showNav} />
-      <div className="mobile__bg__circle absolute top-[-190px] left-[10px] opacity-50 blur-2xl w-[300px] h-[300px] rounded-full bg-[#8d8dda] -z-20"></div>
-      <div className={`mobile__bg__circle absolute top-[610px] right-[0]  opacity-50  blur-2xl w-[150px] h-[150px] rounded-full bg-[#ABD9D9] -z-20 `}></div>
+      <div className="mobile__bg__circle absolute top-[-190px] left-[10px] opacity-50 blur-3xl w-[300px] h-[300px] rounded-full bg-[#8d8dda] -z-20"></div>
+      <div className="mobile__bg__circle absolute top-[610px] right-[0]  opacity-50  blur-3xl w-[150px] h-[150px] rounded-full bg-[#ABD9D9] -z-20 "></div>
       
 
-      <div className="desktop__bg__circle absolute top-[-190px] left-[10px] opacity-50  blur-2xl w-[300px] h-[300px] rounded-full bg-[#8d8dda] -z-20"></div>
-      <div className="desktop__bg__circle absolute top-[-150px] left-[10px] opacity-50  blur-2xl   w-[300px] h-[300px] rounded-full bg-[#8d8dda] -z-20"></div>
+      <div className="desktop__bg__circle absolute top-[-350px] left-[100px] opacity-50 blur-3xl w-[600px] h-[600px] rounded-full bg-[#ABD9D9] -z-20"></div>
+      <div className="desktop__bg__circle absolute top-[610px] right-[250px]  opacity-50  blur-3xl w-[300px] h-[300px] rounded-full bg-[#8d8dda] -z-20 "></div>
 
       <Header showNav={showNav} setShowNav={setShowNav} />
 
