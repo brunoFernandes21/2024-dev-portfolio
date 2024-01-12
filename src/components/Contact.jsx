b4c2f77e5b4ef48019a1d92f3c0be82feaec2d0c
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-01-12 09:51:22 +0000

    update contact page design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,10 +4,10 @@ import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section id="contact" className="contact mt-14 rounded-lg shadow-md bg-slate-900 shadow-white relative">
-      <div className="contact__content">
+    <section id="contact" className="contact mt-14 rounded-lg5 relative ">
+      <div className="contact__content ">
         <h2>Contact</h2>
-        <div className="contact__info mt-4">
+        <div className="contact__info mt-4 shadow-md bg-slate-900 shadow-white p-4 rounded-lg">
           <p className="text-sm">
             Looking for a <strong>Junior Software Developer</strong> to join
             your team?
@@ -18,7 +18,7 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className=" contact__socials gap-4">
+      <div className=" contact__socials gap-4 p-4">
         <h2 className="text-lg">Socials</h2>
         <div className="socials__icons flex gap-4">
           <Link to="https://github.com/brunoFernandes21" target="_blank">
