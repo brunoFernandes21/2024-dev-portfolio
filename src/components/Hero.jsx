2f51679bda0d2949c184739e0e01d89f04ec1f74
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-01-14 00:12:47 +0000

    add new extra content to hero section and adjust the spacing between elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,14 @@ import { FaArrowDown } from "react-icons/fa";
 const Hero = () => {
   return (
     <section className="hero__section ">
-      <div>
+      <div className="hero__image">
         <img
           className="hero__image"
           src="/hero-image-4.svg"
           alt="Illustration of a laptop and a mobile phone"
         />
       </div>
-      <div className="hero__text ">
+      <div className="hero__text">
         <h1>Hello, I am Bruno Fernandes</h1>
         <p className="mt-6">
           I am a <strong>Junior Software Developer</strong> With a passion for{" "}
@@ -25,6 +25,26 @@ const Hero = () => {
           </strong>
           <FaArrowDown />
         </button>
+      </div>
+
+      <div className="online__courses rounded-lg ">
+        <div>
+          <h2>Courses</h2>
+          <div className="courses">
+            <p>Applied Computer Science</p>
+            <p>Software Engineering Bootcamp</p>
+          </div>
+        </div>
+        <div >
+          <h2>Online Platforms</h2>
+          <div className="online__platforms">
+          <p>Free Code Camp</p>
+          <p>CodeCademy</p>
+          <p>Codewars</p>
+          <p>Hackerrank</p>
+          <p>Udemy</p>
+          </div>
+        </div>
       </div>
     </section>
   );
